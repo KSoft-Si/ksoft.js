95e4aff814dc79bd29acee8718eabeae95167fd3
developerrebel <cssherwood03@gmail.com>
2018-10-20 20:09:16 -0700

    add bans api

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { images } = require("./lib/apis/images")
-
+const { bans } = require("./lib/apis/bans")
 const ksoftAPI = class ksoftAPI {
     constructor(token){
         this.images = new images(token)
+        this.bans = new bans(token)
+        
     }
 
 }
