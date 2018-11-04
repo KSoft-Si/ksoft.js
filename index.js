efc04eccae99856289a51916cf799a85876a448c
developerrebel <cssherwood03@gmail.com>
2018-11-03 17:36:19 -0700

    implement webhook

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ const { images } = require("./lib/apis/images")
 const { bans } = require("./lib/apis/bans")
 const { kumo } = require("./lib/apis/kumo")
 const { lyrics } = require("./lib/apis/lyrics")
-const ksoftAPI = class ksoftAPI {
-    constructor(token){
+const Webhook = require("./lib/webhook/server")
+/**
+ * @typedef {Object} webhookOptions webhook options
+ * @prop {number} port webhook http server port
+ * @prop {String} Authentication Your webhook authentication
+ */
+class ksoftAPI{
+    /**
+     * @constructor
+     * @param {number} token Your ksoft api token
+     * @param {boolean} useWebhooks Whether you want to use the webhook feature
+     * @param {webhookOptions} webhookOptions webhook options
+     */
+    constructor(token, useWebhooks, webhookOptions){
+       /**
+        * The webhook class if useWebhooks is true
+        * @type {?Webhook}
+        */
+        this.webhook = null
+        if(useWebhooks){
+            this.webhook = new Webhook(webhookOptions.port, webhookOptions.Authentication)
+        }
         this.images = new images(token)
         this.bans = new bans(token)
         this.kumo = new kumo(token)
@@ -13,3 +33,4 @@ const ksoftAPI = class ksoftAPI {
 }
 
 module.exports = ksoftAPI
+//module.exports.webhook = webhook
