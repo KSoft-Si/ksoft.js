c21a1cdf48a4b83f74b2771bc6f6c19ea794aa85
developerrebel <cssherwood03@gmail.com>
2019-02-10 14:28:45 -0700

    finished half of the bans api and updated the youtube and banCreator utilities

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { images } = require('./lib/apis/images');
+//const { images } = require('./lib/apis/images');
 const { bans } = require('./lib/apis/bans');
-const { kumo } = require('./lib/apis/kumo');
-const { lyrics } = require('./lib/apis/lyrics');
-const music = require('./lib/apis/music');
+//const { kumo } = require('./lib/apis/kumo');
+//const { lyrics } = require('./lib/apis/lyrics');
+//const music = require('./lib/apis/music');
 const BanCreator = require('./lib/util/banCreator');
-const Webhook = require('./lib/webhook/server');
+//const Webhook = require('./lib/webhook/server');
 /**
  * @typedef {Object} webhookOptions webhook options
  * @prop {Boolean} useWebhooks whether to use the webhook feature
@@ -22,18 +22,22 @@ class KsoftAPI {
 		 * The webhook class if useWebhooks is true
 		 * @type {Webhook?}
 		 */
-		if(typeof token !== "string") throw new Error("[Ksoft API] Please make sure your token is a string");
+		if (typeof token !== 'string')
+			throw new Error('[Ksoft API] Please make sure your token is a string');
 		this.webhook = null;
 		if (webhookOptions) {
 			if (webhookOptions.useWebhooks) {
-				this.webhook = new Webhook(webhookOptions.port, webhookOptions.authentication);
+				this.webhook = new Webhook(
+					webhookOptions.port,
+					webhookOptions.authentication
+				);
 			}
 		}
-		this.images = new images(token);
+		//this.images = new images(token);
 		this.bans = new bans(token);
-		this.kumo = new kumo(token);
-		this.lyrics = new lyrics(token);
-		this.music = new music(token);
+		///this.kumo = new kumo(token);
+		//this.lyrics = new lyrics(token);
+		//this.music = new music(token);
 		this.CreateBan = BanCreator;
 	}
 }
