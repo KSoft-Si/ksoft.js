a576424f2b535382de05a1fe86a7aee0c2697600
Raven <marinus@reimer.world>
2019-06-29 18:27:41 +0200

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fetch = require('centra');
 const { readdirSync } = require('fs');
 const { join } = require('path');
+const { EventEmitter } = require('events');
 
 const Ban = require('./lib/util/Ban');
 const Webhook = require('./lib/webhook/server');
@@ -12,12 +13,14 @@ const Webhook = require('./lib/webhook/server');
  * @prop {String} authentication Your webhook authentication
  */
 class KsoftAPI extends EventEmitter {
+
 	/**
 	 * @constructor
 	 * @param {String} token Your KSoft API token
 	 * @param {webhookOptions} [webhookOptions] Only needed if you are using webhooks
 	 */
 	constructor(token, options = {}) {
+		super();
 
 		this.token = token;
 		this.baseURL = 'https://api.ksoft.si';
@@ -27,30 +30,30 @@ class KsoftAPI extends EventEmitter {
 		 * The webhook class if useWebhooks is true
 		 * @type {Webhook?}
 		 */
-		if (typeof token !== 'string')
-			throw new TypeError('[KSoft API] Please make sure your token is a string');
+		if (typeof token !== 'string') { throw new TypeError('[KSoft API] Please make sure your token is a string'); }
 		this.webhook = null;
 		if (options.useWebhooks) {
 			this.webhook = new Webhook(
-				webhookOptions.port,
-				webhookOptions.authentication
+				options.port,
+				options.authentication
 			);
 		}
 
-		this._apis = readdirSync(apiStore).map(i => i.replace('.js', ''));
-		this.api = fetch(baseURL).header('Authorization', `Bearer ${this.token}`);
+		this._apis = readdirSync(this.apiStore).map(i => i.replace('.js', ''));
+		this.api = fetch(this.baseURL).header('Authorization', `Bearer ${this.token}`);
 	}
+
 }
 
 const handler = {
-	get: function(obj, prop) {
+	get: function (obj, prop) {
 		if (obj._apis.includes(prop)) {
-			return require(join(apiStore), prop)(this);
+			return require(join(this.apiStore), prop)(this);
 		} else {
 			throw new SyntaxError(`[KSoft API] ${prop} is not a valid API path.`);
 		}
 	}
-}
+};
 
 const KsoftAPIClient = new Proxy(KsoftAPI, handler);
 
