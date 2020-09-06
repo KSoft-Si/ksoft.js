07f4b565e8d9a2382cbf696810ae323b70ed6cfb
Ravy <ravy@farfrom.earth>
2020-09-06 23:46:49 +0200

    remove all jsdoc since typings pt. 2

--- src/endpoints/kumo/convert.js
+++ src/endpoints/kumo/convert.js
@@ -3,13 +3,6 @@ const Conversion = require('../../../lib/structures/Conversion');
 
 module.exports = class extends Endpoint {
 
-	/**
-	 * Convert one currency to another
-	 * @param {number} value - The currency amount
-	 * @param {string} from - The currency you're converting from
-	 * @param {string} to - The currency you're converting to
-	 * @returns {Conversion}
-	 */
 	async run(value, from, to) {
 		return this.client.api.kumo.currency.get({ value, from, to });
 	}
